dd2595fd9c64daab812eec8b81062d025a21889d
Shen Tian <biozinc@gmail.com>
2017-05-07 20:15:38 +0200

    Adding breathing and heartbeat effects

--- src/lumo/core.clj
+++ src/lumo/core.clj
@@ -5,6 +5,8 @@
             [clj-opc.core :as opc])
   (:gen-class))
 
+;; Some time of day lookups
+
 (defn get-hue
   [hour]
   (cond
@@ -52,6 +54,56 @@
                           (* 100 (dist (/ % len) pos)))
                (range len)))))
 
+(defn breath
+  "Breathing function. Period 1, Range [0,1]
+  http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/"
+  [x]
+  (let [e-inv (/ 1 Math/E)]
+    (/
+     (- (Math/exp (Math/sin (* x 2 Math/PI))) e-inv)
+     (- Math/E e-inv))))
+
+(defn bounce
+  [x lim]
+  (if (< x lim) x (- (* 2 lim) x)))
+
+(defn heart
+  "Heartbeat effect. A double bounce, followed by a pause.
+  1 beat per second. Range [0,1]"
+  [x]
+  (let [xx (mod x 1)
+        beat 0.8]
+    (if (< xx beat)
+      (-> (* 3 (/ 1 beat) xx)
+          (bounce 1.5)
+          (bounce 1))
+      0)))
+
+(defn breath-map
+  [t]
+  (let [len 50
+        sec (+ (* (t/milli t) 0.001) (t/second t))
+        base-color (c/create-color {:h 180
+                                    :s 40
+                                    :l (* (breath (/ sec 3)) 100)})
+        pos (/ (mod sec 10.0) 10.0)]
+    (map color->word
+          (map #(c/darken base-color
+                          (* 100 (dist (/ % len) pos)))
+               (range len)))))
+
+(defn heart-map
+  [t]
+  (let [len 50
+        sec (+ (* (t/milli t) 0.001) (t/second t))
+        base-color (c/create-color {:h 0
+                                    :s 80
+                                    :l (* (heart (* 0.67 sec)) 60)})]
+    (map color->word
+         (map #(c/darken base-color
+                         (* 50 (dist (/ % len) 0)))
+              (range len)))))
+
 (defn set-strip
   [client time]
   (opc/show! client (map-to-strip time)))
@@ -61,6 +113,14 @@
   (let [now (t/now)]
     (opc/show! client (map-to-strip now))
     true))
+
+(defn run-pattern
+  [client pattern-map]
+  (while true
+    (do
+      (let [now (t/now)]
+        (opc/show! client (pattern-map now)))
+      (Thread/sleep 33))))
 
 (defn run
   [client]
