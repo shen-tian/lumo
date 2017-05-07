c194c094de2f66478bbcf4827d65bef7d22f02d2
Shen Tian <biozinc@gmail.com>
2017-05-07 16:33:03 +0200

    Updating color pattern

--- src/lumo/core.clj
+++ src/lumo/core.clj
@@ -5,6 +5,30 @@
             [clj-opc.core :as opc])
   (:gen-class))
 
+(defn get-hue
+  [hour]
+  (cond
+    (< hour 6)  150 ;;"pre-dawn" green
+    (< hour 8)  180 ;;"dawn"     teal
+    (< hour 17) 60  ;;"day"      yellow
+    (< hour 20) 300 ;;"dusk"     pink
+    (< hour 24) 270 ;;"evening"  purple
+    :else "wot"))
+
+(defn get-lumin
+  [hour]
+  (cond
+    (< hour 6) 10
+    (< hour 8) 50
+    (< hour 17) 10
+    (< hour 20) 50
+    (< hour 24) 30
+    :else 0))
+
+(defn utc->sast
+  [date]
+  (t/to-time-zone  date (t/time-zone-for-offset 2)))
+
 (defn color->word
   [color]
   {:r (c/red color) :g (c/green color) :b (c/blue color)})
@@ -19,14 +43,18 @@
   [t]
   (let [len 50
         sec (+ (* (t/milli t) 0.001) (t/second t))
-        base-color (c/create-color {:h (* sec 6)
+        base-color (c/create-color {:h (get-hue (t/hour (utc->sast t)))
                                     :s 50
-                                    :l 50})
+                                    :l (get-lumin (t/hour (utc->sast t)))})
         pos (/ (mod sec 10.0) 10.0)]
     (map color->word
           (map #(c/darken base-color
                           (* 100 (dist (/ % len) pos)))
                (range len)))))
+
+(defn set-strip
+  [client time]
+  (opc/show! client (map-to-strip time)))
 
 (defn tick
   [client]
